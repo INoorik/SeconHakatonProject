b007c24138897c0a0f88f565adef9f7502926650
Razorlanca1 <kartem423@gmail.com>
2024-03-22 16:14:26 +0300

    yandex_id

--- main.py
+++ main.py
@@ -3,18 +3,34 @@ from urllib.parse import unquote
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from yandexid import *
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/login")
+async def read_root(request: Request):
+    code = request.query_params["code"]
+    yandex_oauth = YandexOAuth(
+        client_id='b8896b95d4e4486c98f09dedce538379',
+        client_secret='106fbd36a6e04b4cb2eecddb2281c59c',
+        redirect_uri='http://127.0.0.1:5000/login'
+    )
+    token = yandex_oauth.get_token_from_code(code)
+    yandex_id = YandexID(token.access_token)
+    js = yandex_id.get_user_info_json()
+    print(js)
+    return js
+
+
 @app.get("/calc", response_class=HTMLResponse)
-def calc(request: Request):
+async def calc(request: Request):
     ex = " "
     try:
         ex = unquote(request.url.query)
