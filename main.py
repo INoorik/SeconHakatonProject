3530d64fcf93d33a9562ac6899332dd9ff43cc5b
Razorlanca1 <kartem423@gmail.com>
2024-03-22 17:09:18 +0300

    login

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import token
 from urllib.parse import unquote
 
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Response, Cookie
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from yandexid import *
 
@@ -10,12 +11,42 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+async def main_page(response: Response, request: Request):
+    cookie = request.cookies
+    is_login = "token" in cookie
+    login = ""
+    auth_url = "/"
+
+    try:
+        if is_login:
+            access_token = cookie["token"]
+            yandex_id = YandexID(access_token)
+            js = yandex_id.get_user_info_json()
+            login = js.login
+    except Exception:
+        is_login = False
+
+    if not is_login:
+        yandex_oauth = YandexOAuth(
+            client_id='b8896b95d4e4486c98f09dedce538379',
+            client_secret='106fbd36a6e04b4cb2eecddb2281c59c',
+            redirect_uri='http://127.0.0.1:5000/login'
+        )
+        auth_url = yandex_oauth.get_authorization_url()
+
+    return templates.TemplateResponse("html/main.html", {"request": request, "is_login": is_login,
+                                                         "login_name": login, "login_ref": auth_url})
+
+
+@app.get("/logout")
+async def logout(response: Response, request: Request):
+    response = RedirectResponse("/")
+    response.delete_cookie("token")
+    return response
 
 
 @app.get("/login")
-async def read_root(request: Request):
+async def login(response: Response, request: Request):
     code = request.query_params["code"]
     yandex_oauth = YandexOAuth(
         client_id='b8896b95d4e4486c98f09dedce538379',
@@ -23,19 +54,6 @@ async def read_root(request: Request):
         redirect_uri='http://127.0.0.1:5000/login'
     )
     token = yandex_oauth.get_token_from_code(code)
-    yandex_id = YandexID(token.access_token)
-    js = yandex_id.get_user_info_json()
-    print(js)
-    return js
-
-
-@app.get("/calc", response_class=HTMLResponse)
-async def calc(request: Request):
-    ex = " "
-    try:
-        ex = unquote(request.url.query)
-        res = eval(ex)
-    except Exception:
-        return templates.TemplateResponse('main.html', {"request": request, 'req': ex, 'err': True})
-
-    return templates.TemplateResponse("main.html", {"request": request, "req": ex, 'res': res, 'err': False})
+    response = RedirectResponse("/")
+    response.set_cookie(key="token", value=token.access_token)
+    return response
