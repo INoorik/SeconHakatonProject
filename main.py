e01694aed78bcca068e17206f40b007c1aab7dd9
INoorik <ajnurabuzarov8@gmail.com>
2024-03-24 20:39:52 +0300

    main.py links updated

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def get_user(request):
         yandex_oauth = YandexOAuth(
             client_id='b8896b95d4e4486c98f09dedce538379',
             client_secret='106fbd36a6e04b4cb2eecddb2281c59c',
-            redirect_uri='http://127.0.0.1:5000/set_token'
+            redirect_uri='http://www.seconhakatonctfproject.fun/set_token'
         )
         auth_url = yandex_oauth.get_authorization_url()
 
@@ -107,7 +107,7 @@ async def set_token(response: Response, request: Request):
     yandex_oauth = YandexOAuth(
         client_id='b8896b95d4e4486c98f09dedce538379',
         client_secret='106fbd36a6e04b4cb2eecddb2281c59c',
-        redirect_uri='http://127.0.0.1:5000/set_token'
+        redirect_uri='http://www.seconhakatonctfproject.fun/set_token'
     )
     token = yandex_oauth.get_token_from_code(code)
     response = RedirectResponse("/login")
