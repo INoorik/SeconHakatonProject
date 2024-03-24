464a93f0d30ed73d64a7b4c2d774797ec4355d56
INoorik <ajnurabuzarov8@gmail.com>
2024-03-24 19:37:45 +0300

    run_server.py port changed

--- run_server.py
+++ run_server.py
@@ -4,4 +4,4 @@ from database_api import create_tables, database_connection
 
 if __name__ == "__main__":
     create_tables(database_connection)
-    uvicorn.run(fastapi_app, host="127.0.0.1", port=5000, log_level="info")
+    uvicorn.run(fastapi_app, host="127.0.0.1", port=8000, log_level="info")
