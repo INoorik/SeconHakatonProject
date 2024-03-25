d937f5028adc463854037e3685105244d7b9d2a7
INoorik <ajnurabuzarov8@gmail.com>
2024-03-25 10:36:41 +0300

    reload=True setted in uvicorn

--- run_server.py
+++ run_server.py
@@ -4,4 +4,4 @@ from database_api import create_tables, database_connection
 
 if __name__ == "__main__":
     create_tables(database_connection)
-    uvicorn.run(fastapi_app, host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run(fastapi_app, host="0.0.0.0", port=8000, log_level="info", reload=True)
